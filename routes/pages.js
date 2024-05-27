776b93f80e112d4514666faf314f4f8cb0c0a6cc
𝕷𝖚𝖒𝖊𝖓 𝕻𝖊𝖆𝖗𝖘𝖔𝖓 <139921639+lumenpearson@users.noreply.github.com>
2024-05-27 23:54:20 +0300

    patch: update pages.js

--- routes/pages.js
+++ routes/pages.js
@@ -3,6 +3,6 @@ const { sendIndex, sendDashboard } = require("../controllers/auth");
 const { checkCookiesJWT, checkAuth } = require("../middlewares/auth");
 
 pagesRouter.get("/", sendIndex);
-pagesRouter.get("/admin/**", checkCookiesJWT, checkAuth, sendDashboard);
+pagesRouter.get("/admin", checkCookiesJWT, checkAuth, sendDashboard);
 
 module.exports = pagesRouter;
